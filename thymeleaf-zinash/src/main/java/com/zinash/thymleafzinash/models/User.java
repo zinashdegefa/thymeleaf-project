107487a784ef588a08f5b7b344b1b8518a42cb21
zinashdegefa <mydearbarkot@gmail.com>
2024-03-16 14:09:25 -0500

    save and update

--- thymeleaf-zinash/src/main/java/com/zinash/thymleafzinash/models/User.java
+++ thymeleaf-zinash/src/main/java/com/zinash/thymleafzinash/models/User.java
@@ -1,5 +1,8 @@
 package com.zinash.thymleafzinash.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +15,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String firstName;
     private String lastName;
